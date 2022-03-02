8f5d9c7d92c1b4e8df80ab5590576dab28c5c5cc
sandeep <sandeep.samal@softsuave.com>
2022-03-02 12:48:17 +0530

    - Added react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>SubTest :</h2>
+        Sub module
+      </div>
+    </div>
+  );
+}
+
+export default App;
